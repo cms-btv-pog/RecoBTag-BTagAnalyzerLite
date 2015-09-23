76f10c3d053b7786e8b0d9186fcc33a8b4025dee
Caterina <caterina.vernieri@cern.ch>
2015-09-23 11:29:11 -0500

    update

--- python/bTagAnalyzerLiteCommon_cff.py
+++ python/bTagAnalyzerLiteCommon_cff.py
@@ -1,6 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
+from RecoBTag.SecondaryVertex.trackSelection_cff import *
+from RecoBTag.SecondaryVertex.trackPseudoSelection_cff import *
+
+trackPseudoSelectionBlock_fat= trackPseudoSelectionBlock.trackPseudoSelection.clone(jetDeltaRMax=cms.double(1))
+trackSelectionBlock_fat= trackSelectionBlock.trackSelection.clone(jetDeltaRMax=cms.double(1))
+
 bTagAnalyzerLiteCommon = cms.PSet(
+    #trackPseudoSelectionBlock_fat,
     runFatJets               = cms.bool(False),
     runSubJets               = cms.bool(False),
     allowJetSkipping         = cms.bool(True),
@@ -27,6 +34,29 @@ bTagAnalyzerLiteCommon = cms.PSet(
     R0                       = cms.double(0.8),
     maxSVDeltaRToJet         = cms.double(0.7),
     trackPairV0Filter = cms.PSet(k0sMassWindow = cms.double(0.03)),
+    trackSort = cms.string( "sip2dSig" ),
+    trackSelection = cms.PSet(
+                variableJTAPars,
+		totalHitsMin = cms.uint32(0),
+		jetDeltaRMax = cms.double(1),
+		qualityClass = cms.string('any'),
+		pixelHitsMin = cms.uint32(0),
+		maxDistToAxis = cms.double(0.07),
+		maxDecayLen = cms.double(5),
+		sip3dSigMin = cms.double(-99999.9),
+		sip3dSigMax = cms.double(99999.9),
+		sip2dValMax = cms.double(99999.9),
+		ptMin = cms.double(0.0),
+		sip2dSigMax = cms.double(99999.9),
+		sip2dSigMin = cms.double(-99999.9),
+		sip3dValMax = cms.double(99999.9),
+		sip3dValMin = cms.double(-99999.9),
+		sip2dValMin = cms.double(-99999.9),
+		normChi2Max = cms.double(99999.9),
+                useVariableJTA = cms.bool(False)        
+                ),
+#    trackSelection = cms.PSet( jetDeltaRMax=cms.double(1)),
+    #pseudoVertexV0Filter = cms.PSet(k0sMassWindow = cms.double(0.05)),
     TriggerPathNames = cms.vstring(
         "HLT_HT750_v*"
     )
